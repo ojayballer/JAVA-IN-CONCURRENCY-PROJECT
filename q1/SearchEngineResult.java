a1cfd8aa404a73d243342371fbc0698b44886cd4
Okikiola <okikday@gmail.com>
2026-02-06 14:56:50 +0100

    little initialization commit

---
+++ q1/SearchEngineResult.java
@@ -0,0 +1,24 @@
+class SearchEngineResult{
+    private String title;
+    private String url;
+    private String description;
+
+    public SearchEngineResult(String title, String url, String description) {
+        this.title = title;
+        this.url = url;
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    
+}
